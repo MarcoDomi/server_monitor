9505b65ccdec4f21162104499d0924282952ada5
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-12-20 17:50:43 -0600

    move inputs inside try block

--- add.py
+++ add.py
@@ -2,24 +2,24 @@ import pickle
 from serverChecker import server
 
 
-name = input("Enter server name: ")
-port = input("Enter port number: ")
-type = input("Enter connection type: ")
-
-new_server = server(name, port, type)
-
 file_name = "server_data.pickle"
 try:
     file = open(file_name, 'rb')
     servers = pickle.load(file)
     file.close()
 
+    name = input("Enter server name: ")
+    port = input("Enter port number: ")
+    type = input("Enter connection type: ")
+
+    new_server = server(name, port, type)
+
     servers.append(new_server)
 
     file = open(file_name, 'wb')
     pickle.dump(servers, file)
     file.close()
-
+    print("New server added!")
 
 except FileNotFoundError:
     print(f"File Error: {file_name} does not exist")
